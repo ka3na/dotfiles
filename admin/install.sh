4e3e53fcb8ba3621e275d9f66c369e04e5d1dc2f
ka3na <mka3na@gmail.com>
2020-09-22 19:58:33 +0100

    feat(sync): remote script testing fixes

--- admin/install.sh
+++ admin/install.sh
@@ -34,9 +34,9 @@ cd ~
 echo $styleAction" Checking if required path '$DIR_ROOT' is available "$styleEnd
 if [ -e "$DIR_ROOT" ]; then
   if [ -d "$DIR_ROOT" ]; then
-    echo $styleWarning"Required '$DIR_ROOT' path already exists and is a folder"$styleEnd
+    echo $styleInfo"Required '$DIR_ROOT' path already exists and is a folder"$styleEnd
   else
-    echo $styleWarning"Required '$DIR_ROOT' path already exists and is a file"$styleEnd
+    echo $styleInfo"Required '$DIR_ROOT' path already exists and is a file"$styleEnd
   fi
   echo $styleInfo"Backing up into '$DIR_BACKUP'"$styleEnd
   mkdir -p "$DIR_BACKUP"
@@ -52,10 +52,10 @@ git clone git@github.com:ka3na/dotfiles.git "$DIR_ROOT"
 echo $styleConfirm"...done"$styleEnd
 
 # Call the sync (setup) command
-echo $styleAction" Calling the sync command to set up our dotfiles "$styleEnd
+echo $styleAction" Calling the 'sync' script to set up our dotfiles "$styleEnd
 fileSync=dotfiles/admin/sync.sh
 if [ -f "$fileSync" ]; then
-  echo $styleConfirm"...sync file found"$styleEnd
+  echo $styleConfirm"...script file found"$styleEnd
   source $fileSync
 else 
   echo $styleError"Error: unable to complete the action, $fileSync does not exist"$styleEnd
