b5207acb680cd4498459b0fec06dc1aa98a0455f
ka3na <mka3na@gmail.com>
2020-09-24 19:24:35 +0100

    fix(admin): unify shebang

--- admin/sync.sh
+++ admin/sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # ------------------------------------------------------------------------------
 # Sync dotfiles from local or remote.
--- helpers/textFormat.sh
+++ helpers/textFormat.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # ------------------------------------------------------------------------------
 # Text output colours and formats
